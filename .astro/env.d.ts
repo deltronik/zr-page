4992814f80e45ee2a46538e3d24964920a08b7b3
DANH <deltronik98@gmail.com>
2025-11-24 13:01:50 -0300

    fix: eliminación de archivos no necesarios para prod

--- .astro/env.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'astro:env/server' {
-	export const TURSO_DATABASE_URL: string;	
-	export const TURSO_AUTH_TOKEN: string;	
-	export const MERCADOPAGO_ACCESS_TOKEN: string;	
-	export const MERCADOPAGO_PUBLIC_KEY: string;	
-}
